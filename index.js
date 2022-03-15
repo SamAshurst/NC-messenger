222c66fc7a07c0fd72fbb6b7d25f41bf50d0025a
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-03-15 12:09:00 +0000

    Setting up ESLint

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 const server = http.createServer(app);
-const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
-  });
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+    socket.on("chat message", (msg) => {
+        console.log(`message: ${msg}`);
+        io.emit("chat message", msg);
+    });
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
 });
 io.on("connection", (socket) => {
-  socket.emit("hello");
+    socket.emit("hello");
 });
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+    console.log("listening on *:3000");
 });
